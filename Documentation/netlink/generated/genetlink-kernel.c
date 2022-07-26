2aeb74a97eec88cc5b48f8f36237e45084f77c37
Jakub Kicinski <kuba@kernel.org>
2022-07-25 17:26:29 -0700

    flip source vs header
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- Documentation/netlink/generated/genetlink-kernel.c
+++ Documentation/netlink/generated/genetlink-kernel.c
@@ -6,44 +6,22 @@
 
 #include <linux/genetlink.h>
 
-// Common nested types
-struct genlctrl_operation {
-	u32 op_id_present:1;
-	u32 op_flags_present:1;
-
-	u32 op_id;
-	u32 op_flags;
-};
-void genlctrl_operation_free(struct genlctrl_operation *req);
-
 // CTRL_CMD_GETFAMILY
-struct genlctrl_getfamily_req {
-	u32 family_id_present:1;
-	u32 family_name_present:1;
+void genlctrl_getfamily_req_parse(const struct nlattr **tb, struct genlctrl_getfamily_req *req)
+{
+	if (tb[CTRL_ATTR_FAMILY_ID]) {
+		req->family_id_present = 1;
+		req->family_id = nla_get_u16(tb[CTRL_ATTR_FAMILY_ID]);
+	}
+	if (tb[CTRL_ATTR_FAMILY_NAME]) {
+		req->family_name_present = 1;
+		strcpy(req->family_name, nla_data(tb[CTRL_ATTR_FAMILY_NAME]));
+	}
+}
 
-	u16 family_id;
-	char family_name[GENL_NAMSIZ];
+const struct nla_policy genlctrl_getfamily_policy[] = {
+	[CTRL_ATTR_FAMILY_ID] = { .type = NLA_U16 },
+	[CTRL_ATTR_FAMILY_NAME] = { .type = NLA_NUL_STRING, .len = GENL_NAMSIZ - 1 },
 };
-void genlctrl_getfamily_req_free(struct genlctrl_getfamily_req *req);
-
-struct genlctrl_getfamily_rsp {
-	u32 family_id_present:1;
-	u32 family_name_present:1;
-	u32 version_present:1;
-	u32 hdrsize_present:1;
-	u32 maxattr_present:1;
-	u32 ops_present:1;
-
-	u16 family_id;
-	char family_name[GENL_NAMSIZ];
-	u32 version;
-	u32 hdrsize;
-	u32 maxattr;
-	unsigned int n_ops;
-	struct genlctrl_operation *ops;
-};
-void genlctrl_getfamily_rsp_free(struct genlctrl_getfamily_rsp *req);
-void genlctrl_getfamily_req_parse(const struct nlattr **tb, struct genlctrl_getfamily_req *req);
-const struct nla_policy genlctrl_getfamily_policy[];
 
 // CTRL_CMD_GETPOLICY
--- Documentation/netlink/generated/genetlink-user.h
+++ Documentation/netlink/generated/genetlink-user.h
@@ -4,10 +4,43 @@
 
 #include <linux/genetlink.h>
 
+// Common nested types
+struct genlctrl_operation {
+	__u32 op_id_present:1;
+	__u32 op_flags_present:1;
+
+	__u32 op_id;
+	__u32 op_flags;
+};
+void genlctrl_operation_free(struct genlctrl_operation *req);
+
 // CTRL_CMD_GETFAMILY
-int genlctrl_getfamily(struct ynl_sock *ys, struct genlctrl_getfamily_req *req)
-{
-	return 0;
-}
+struct genlctrl_getfamily_req {
+	__u32 family_id_present:1;
+	__u32 family_name_present:1;
+
+	__u16 family_id;
+	char family_name[GENL_NAMSIZ];
+};
+void genlctrl_getfamily_req_free(struct genlctrl_getfamily_req *req);
+
+struct genlctrl_getfamily_rsp {
+	__u32 family_id_present:1;
+	__u32 family_name_present:1;
+	__u32 version_present:1;
+	__u32 hdrsize_present:1;
+	__u32 maxattr_present:1;
+	__u32 ops_present:1;
+
+	__u16 family_id;
+	char family_name[GENL_NAMSIZ];
+	__u32 version;
+	__u32 hdrsize;
+	__u32 maxattr;
+	unsigned int n_ops;
+	struct genlctrl_operation *ops;
+};
+void genlctrl_getfamily_rsp_free(struct genlctrl_getfamily_rsp *req);
+int genlctrl_getfamily(struct ynl_sock *ys, struct genlctrl_getfamily_req *req);
 
 // CTRL_CMD_GETPOLICY
